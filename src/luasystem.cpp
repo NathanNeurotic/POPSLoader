930b684e7f2ed4828cb03a5d08e976dd6e60dcdb
NathanNeurotic (Ripto) <109461996+NathanNeurotic@users.noreply.github.com>
2026-01-14 15:59:08 -0800

    Use POSIX flags for fileXio and fix dprintf warning

--- src/luasystem.cpp
+++ src/luasystem.cpp
@@ -385,6 +385,7 @@ static int lua_dprintf(lua_State *L)
 	if (argc != 1) return luaL_error(L, "System.dprintf(text) expects one string");
 	const char *text = luaL_checkstring(L, 1);
 	dbgprintf("%s", text);
+	(void)text;
 	return 0;
 }
 
@@ -1007,30 +1008,6 @@ void luaSystem_init(lua_State *L) {
 
 	lua_pushinteger(L, SEEK_CUR);
 	lua_setglobal(L, "CUR");
-
-	lua_pushinteger(L, FIO_O_RDONLY);
-	lua_setglobal(L, "FXO_RDONLY");
-
-	lua_pushinteger(L, FIO_O_WRONLY);
-	lua_setglobal(L, "FXO_WRONLY");
-
-	lua_pushinteger(L, FIO_O_RDWR);
-	lua_setglobal(L, "FXO_RDWR");
-
-	lua_pushinteger(L, FIO_O_CREAT);
-	lua_setglobal(L, "FXO_CREAT");
-
-	lua_pushinteger(L, FIO_O_TRUNC);
-	lua_setglobal(L, "FXO_TRUNC");
-
-	lua_pushinteger(L, FIO_SEEK_SET);
-	lua_setglobal(L, "FX_SEEK_SET");
-
-	lua_pushinteger(L, FIO_SEEK_END);
-	lua_setglobal(L, "FX_SEEK_END");
-
-	lua_pushinteger(L, FIO_SEEK_CUR);
-	lua_setglobal(L, "FX_SEEK_CUR");
 
 	lua_pushinteger(L, 1);
 	lua_setglobal(L, "READ_ONLY");
