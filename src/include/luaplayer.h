9514298250aab0abdb0cd8c9e42eaf17a7cf04a9
NathanNeurotic (Ripto) <109461996+NathanNeurotic@users.noreply.github.com>
2026-01-15 13:12:20 -0800

    Revert non-upstream reset tweaks

--- src/include/luaplayer.h
+++ src/include/luaplayer.h
@@ -27,7 +27,7 @@ int getBootDevice(void);
 
 extern size_t GetFreeSize(void);
 
-extern const char * runScript(const char* script, bool isStringBuffer);
+extern const char * runScript(const char* script, bool isStringBuffer, size_t buffer_len);
 extern void luaC_collectgarbage (lua_State *L);
 
 //extern void luaSound_init(lua_State *L);
