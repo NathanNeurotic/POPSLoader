b1adc3da90df0a6799aaf33c4c90cc281cfd8a38
NathanNeurotic (Ripto) <109461996+NathanNeurotic@users.noreply.github.com>
2026-01-26 15:21:07 -0800

    Add files via upload

---
+++ bin/POPSLDR/IMG/images.lua
@@ -0,0 +1,68 @@
+--[[
+  ___  ___  ___  ___ _                 _         
+ | _ \/ _ \| _ \/ __| |   ___  __ _ __| |___ _ _ 
+ |  _/ (_) |  _/\__ \ |__/ _ \/ _` / _` / -_) '_|
+ |_|  \___/|_|  |___/____\___/\__,_\__,_\___|_|  
+  Licensed under GNU General public license v3.0
+--]]
+
+LOG("Registering images")
+local function ResolveImage(name)
+  return System.resolveAssetType(name, ASSET_IMG) or name
+end
+--- Add your images to this table, just write the name of the file.
+--- Flat layout places images beside POPSLOADER.ELF; legacy installs can still use POPSLDR/IMG/*
+--- FILES MUST HAVE EXTENSION. filename is parsed to create the access key: USB.PNG will be accesed by typing `IMG["USB"]`
+local IMGS = {
+  "USB.png",
+  "SMB.png",
+  "MMCE.png",
+  "MX4SIO.png",
+  "HDD.png",
+  "APAHDD.png",
+  "BDHDD.png",
+  "BKG.png",
+  "MISSING.png",
+  "PSL.png",
+  "select.png",
+  "start.png",
+  "triangle.png",
+  "circle.png",
+  "cross.png",
+  --"down.png",
+  --"L1.png",
+  --"L2.png",
+  --"L3.png",
+  --"left.png",
+  --"R1.png",
+  --"R2.png",
+  --"R3.png",
+  --"right.png",
+  "square.png",
+  --"up.png",
+}
+local IMG_SOURCES = {}
+for x=1, #IMGS do
+  local key = IMGS[x]:match("(.+)%..+$")
+  IMG_SOURCES[key] = IMGS[x]
+end
+IMG_SOURCES["MMCE"] = "MMCE.png"
+IMG_SOURCES["MX4SIO"] = "MX4SIO.png"
+
+IMG = setmetatable({}, {
+  __index = function (tbl, key)
+    local source = IMG_SOURCES[key]
+    if source == nil then return nil end
+    if BOOT_PROF and BOOT_PROF.stamp and not BOOT_PROF.textures_ready then
+      BOOT_PROF.textures_ready = true
+      BOOT_PROF.stamp("UI assets init (textures)")
+    end
+    local path = ResolveImage(source)
+    local img = Graphics.loadImage(path)
+    if img == nil then error("Could not load '"..path.."'") end
+    Graphics.setImageFilters(img, LINEAR)
+    rawset(tbl, key, img)
+    return img
+  end
+})
+LOGF("%d images registered (lazy)", #IMGS)
