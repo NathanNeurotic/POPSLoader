ff517194e3922eb306aa08cffffc8f56cee4fc0c
NathanNeurotic (Ripto) <109461996+NathanNeurotic@users.noreply.github.com>
2026-01-29 08:10:53 -0800

    Adjust profile label spacing

--- bin/POPSLDR/ui.lua
+++ bin/POPSLDR/ui.lua
@@ -1746,7 +1746,7 @@ end
           Font.ftPrint(UI.FONT.TITLE, UI.SCR.X_MID, layout.TITLE_Y, 8, UI.SCR.X, 16, "POPSLoader", UI.COLORS.TEXT_PRIMARY)
           local status_y = layout.STATUS_Y - 6
           Font.ftPrint(UI.FONT.STATUS, UI.SCR.X_MID, status_y, 8, UI.SCR.X, 16, "ACTIVE BDMA: "..ResolveActiveBDMALabel(), UI.COLORS.TEXT_PRIMARY)
-          status_y = status_y + 12
+          status_y = status_y + 13
           Font.ftPrint(UI.FONT.STATUS, UI.SCR.X_MID, status_y, 8, UI.SCR.X, 16, "Profile Selected: "..ResolveProfileLabel(), UI.COLORS.TEXT_PRIMARY)
           status_y = status_y + 12
           top_label_y = status_y + 4
