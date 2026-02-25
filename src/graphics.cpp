136b9f0244df3030597bffe76aaf7f59f3c7d4bb
NathanNeurotic (Ripto) <noahtkevans@gmail.com>
2026-02-24 21:41:08 -0800

    Upload CLUT for embedded paletted textures like loadpng

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -1579,13 +1579,13 @@ GSTEXTURE* loadEmbeddedPNG(uint8_t * data, size_t size, bool delayed)
 		}
 
 		// Upload texture
-		gsKit_setup_tbw(tex);
 		if((tex->PSM == GS_PSM_T4 || tex->PSM == GS_PSM_T8) && tex->Clut != NULL)
+			gsKit_texture_upload(gsGlobal, tex);
+		else
 		{
-			tex->ClutPSM = GS_PSM_CT32;
-			tex->ClutStorageMode = GS_CLUT_STORAGE_CSM1;
+			gsKit_setup_tbw(tex);
+			gsKit_texture_upload(gsGlobal, tex);
 		}
-		gsKit_texture_upload(gsGlobal, tex);
 		// Free texture
 		free(tex->Mem);
 		tex->Mem = NULL;
