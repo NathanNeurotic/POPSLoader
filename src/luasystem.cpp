8bab847932e0e4381270cd541263dbe5bad5b1da
NathanNeurotic <noahtkevans@gmail.com>
2026-03-21 10:56:04 -0700

    Investigate POPSTARTER hang

--- src/luasystem.cpp
+++ src/luasystem.cpp
@@ -974,14 +974,12 @@ static int lua_loadELF(lua_State *L)
 		argv_static[1] = NULL;
 		DPRINTF("# Loading ELF argv0='%s' argc=1\n", argv_static[0]);
 		int rc;
-		if (rebootIOP != 0) {
+		if (IsHddOrPfsElfPath(elftoload) && strcmp(selector_buf, elftoload) != 0) {
+			rc = LoadELFFromFileWithPartition(elftoload, NULL, 1, argv_static);
+		} else if (rebootIOP != 0) {
 			rc = LoadELFFromFileExecPS2RebootIOP(elftoload, 1, argv_static);
 		} else if (IsHddOrPfsElfPath(elftoload)) {
-			if (strcmp(selector_buf, elftoload) != 0) {
-				rc = LoadELFFromFileWithPartition(elftoload, NULL, 1, argv_static);
-			} else {
-				rc = LoadELFFromFile(elftoload, 1, argv_static);
-			}
+			rc = LoadELFFromFile(elftoload, 1, argv_static);
 		} else {
 			rc = LoadELFFromFileExecPS2(elftoload, 1, argv_static);
 		}
